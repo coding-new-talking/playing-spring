9ece200f09ce86f4b1ab35398f9e037ba9b3e57d
coding-new-talking <1511937685@qq.com>
2019-10-16 17:37:37 +0800

    update

--- src/main/java/org/cnt/psb/PlayingSbApplication.java
+++ src/main/java/org/cnt/psb/PlayingSbApplication.java
@@ -1,7 +1,6 @@
 package org.cnt.psb;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
@@ -9,9 +8,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class PlayingSbApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(TasteSpringApplication.class, args);
+		//SpringApplication.run(PlayingSbApplication.class, args);
 		SpringApplication sa = new SpringApplication(PlayingSbApplication.class);
-		sa.setWebApplicationType(WebApplicationType.NONE);
 		ConfigurableApplicationContext cac = sa.run(args);
 	}
 
